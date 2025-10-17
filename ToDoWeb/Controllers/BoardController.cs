55fe6e0acc837f1783daf946669f9ba53f2a5d9a
MervAslan <merveaslan044@gmail.com>
2025-10-17 17:53:33 +0300

    Added BoardCreate Modal

--- ToDoWeb/Controllers/BoardController.cs
+++ ToDoWeb/Controllers/BoardController.cs
@@ -1,6 +1,30 @@
-﻿namespace ToDoWeb.Controllers
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ToDo.Application.CQRS.Commands.BoardCommands;
+
+
+namespace ToDoWeb.Controllers
 {
-    public class BoardController
+    public class BoardController : Controller
     {
+        private readonly IMediator _mediator;
+        public BoardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBoard(CreateBoardCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (!result.IsSuccess)
+            {
+                TempData["ErrorMessage"] = result.Message;
+                return RedirectToAction("Boards", "Main");
+            }
+             TempData["SuccessMessage"] = result.Message;
+             return RedirectToAction("Boards", "Main");
+
+
+        }
     }
 }
